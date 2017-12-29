d78d8177ce2fcfd154077769e2ddd97a1baff41f
Ola Vea <ola@olavea.com>
2017-12-29 10:19:14 +0100

    Vdieo 17 Error insights with Sentry

---
+++ reduxstagram17.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import Components
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+import Raven from 'raven.js';
+import { sentry_url } from'./data/config';
+
+Raven.config(sentry_url).install();
+
+// console.log(window.doesNotExist.nope);
+// Open up console cannot read property 'nope' of undefined
+// Go into sentry dashboard and look at error
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
